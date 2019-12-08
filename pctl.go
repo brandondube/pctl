0961cc97a793a157de64049b320985b03d3bfbb8
Brandon Dube <brandondube@gmail.com>
2019-12-08 11:35:53 -0800

    start stop pause instead of closing the channel as only way to stop

--- pctl.go
+++ pctl.go
@@ -37,6 +37,9 @@ type PIDCtl struct {
 
 	// Running indicates if the loop is currently running
 	Running bool
+
+	// signal is the channel used to send commands to the PID loops
+	signal chan string
 }
 
 // LastObs returns the read-only last observation time.
@@ -63,7 +66,33 @@ func (pid *PIDCtl) Locking() bool {
 	return pid.locking
 }
 
-/*Loop runs the PID loop.  It takes a channel of measurements to read from
+// Pause temporarely suspends the PID loop.  The behavior due to a chain of
+// calls Pause() Stop() is undefined.  Always Resume() or exit the program
+// after calling Pause().
+func (pid *PIDCtl) Pause() {
+	pid.signal <- "pause"
+	pid.Running = false
+	// the value of pause and resume isn't really needed for the machine,
+	// but it is more clear for the ape behind the keyboard to understand
+	// what is going on
+}
+
+// Resume restarts the PID loop from suspension without zeroing the state
+func (pid *PIDCtl) Resume() {
+	pid.signal <- "resume"
+	pid.Running = true
+}
+
+// Stop the PID loop.
+//
+// The behavior due to a chain of calls Pause() Stop() is undefined.  Always
+// Resume() or exit the program after calling Pause().
+func (pid *PIDCtl) Stop() {
+	pid.signal <- "stop"
+	pid.Running = false
+}
+
+/*Start runs the PID loop.  It takes a channel of measurements to read from
 and a channel of outputs to write to.  To stop the loop, simply close m.  The
 remaining values in the channel will be exhausted, then the loop exited.
 
@@ -72,16 +101,24 @@ If you desire close (stop) to be immediate, use an unbuffered m channel.
 The first observation in m is used to seed the loop with a fresh measurement.
 The existing output value is sent on o to maintain synchronization.
 
-use go Loop(...) to avoid blocking the calling thread.
+use go Start(...) to avoid blocking the calling thread.
 
 To do multiple things with the output value, fan out the channel
 or provide that logic in the consuming function.
 
-The struct fields or lastObs() may be accessed at any time while the loop
+The struct fields or LastObs() may be accessed at any time while the loop
 is running.  To guarantee that they are in sync, ensure pid.Locking() == true
 and acquire the lock during your read.
 */
-func (pid *PIDCtl) Loop(m <-chan float64, o chan<- float64) {
+func (pid *PIDCtl) Start(m <-chan float64, o chan<- float64) {
+	pid.loop(m, o)
+}
+
+func (pid *PIDCtl) loop(m <-chan float64, o chan<- float64) {
+	// guard against the uninitialized case
+	if pid.signal == nil {
+		pid.signal = make(chan string)
+	}
 	pid.Running = true
 	defer func() { pid.Running = false }()
 	var (
@@ -90,33 +127,43 @@ func (pid *PIDCtl) Loop(m <-chan float64, o chan<- float64) {
 		integral float64 = 0
 		started          = false
 	)
-	for pVal := range m {
-		// update the clock and measurement
-		updateT := time.Now()
-		pid.Meas = pVal
-		// zero time, just update the measurement and output
-		// to seed the loop and don't try to control
-		if !started {
-			pid.lastObs = updateT
+	for {
+		select {
+		case cmd := <-pid.signal:
+			if cmd == "pause" {
+				<-pid.signal // wait for the resume command
+			} else if cmd == "stop" {
+				return
+			}
+
+		case pVal := <-m:
+			// update the clock and measurement
+			updateT := time.Now()
 			pid.Meas = pVal
-			started = true
+			// zero time, just update the measurement and output
+			// to seed the loop and don't try to control
+			if !started {
+				pid.lastObs = updateT
+				pid.Meas = pVal
+				started = true
+				o <- pid.Output
+				continue
+			}
+			if pid.locking {
+				pid.Lock()
+			}
+			dt := updateT.Sub(pid.lastObs).Seconds()
+			err := pid.Setpt - pVal
+			integral += err * dt
+			derivative := (err - prevErr) / dt
+			pid.Output = pid.P*err + pid.I*integral + pid.D*derivative
+			pid.Meas = pVal
+			pid.lastObs = updateT
+			prevErr = err
 			o <- pid.Output
-			continue
-		}
-		if pid.locking {
-			pid.Lock()
-		}
-		dt := updateT.Sub(pid.lastObs).Seconds()
-		err := pid.Setpt - pVal
-		integral += err * dt
-		derivative := (err - prevErr) / dt
-		pid.Output = pid.P*err + pid.I*integral + pid.D*derivative
-		pid.Meas = pVal
-		pid.lastObs = updateT
-		prevErr = err
-		o <- pid.Output
-		if pid.locking {
-			pid.Unlock()
+			if pid.locking {
+				pid.Unlock()
+			}
 		}
 	}
 }
