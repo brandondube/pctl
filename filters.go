7da795669b8cc08bc222ea1390e3ecb40e05d09f
Brandon Dube <brandondube@gmail.com>
2020-08-29 13:08:00 -0700

    + hpf

--- filters.go
+++ filters.go
@@ -55,3 +55,54 @@ func (l *LPF) RC() float64 {
 func (l *LPF) Previous() (float64, time.Time) {
 	return l.prev, l.last
 }
+
+// HPF is a digital discrete-time high pass filter.  It does not require
+// Update() to be called in a regular cadence.
+//
+// The cutoff frequency may not be changed; the Previous() method can retrieve
+// the state of the filter to create a new one.
+type HPF struct {
+	rc   float64
+	fc   float64
+	prev float64
+	last time.Time
+}
+
+// NewHPF returns a new low pass filter with the specified cutoff frequency
+// in Hertz
+func NewHPF(cutoffFreq float64) *HPF {
+	return &HPF{
+		fc: cutoffFreq,
+		rc: 1 / (2 * math.Pi * cutoffFreq)}
+}
+
+// Update processes an input value, returning the filtered output
+func (h *HPF) Update(input float64) float64 {
+	if h.last.IsZero() {
+		// not initialized
+		h.prev = input
+		h.last = time.Now()
+		return input
+	}
+	now := time.Now()
+	dT := (now.Sub(h.last)).Seconds()
+	alpha := h.rc / (h.rc + dT)
+	h.prev = alpha * (h.prev + dT)
+	h.last = now
+	return h.prev
+}
+
+// Cutoff returns the filter's cutoff frequency in Hertz
+func (h *HPF) Cutoff() float64 {
+	return h.fc
+}
+
+// RC returns the filter's RC time constant, 1/(2pi cutoff)
+func (h *HPF) RC() float64 {
+	return h.rc
+}
+
+// Previous returns the last value at the filter's output and when it happened
+func (h *HPF) Previous() (float64, time.Time) {
+	return h.prev, h.last
+}
