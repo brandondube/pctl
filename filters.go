aca28e3a3b550a0cccdd44e8b32895c328de1a9d
Brandon Dube <brandondube@gmail.com>
2020-08-29 12:55:53 -0700

    LowPass => LPF; lpf => filters for HPF too.

--- filters.go
+++ filters.go
@@ -5,28 +5,28 @@ import (
 	"time"
 )
 
-// LowPass is a digital discrete-time low pass filter.  It does not require
+// LPF is a digital discrete-time low pass filter.  It does not require
 // Update() to be called in a regular cadence.
 //
 // The cutoff frequency may not be changed; the Previous() method can retrieve
 // the state of the filter to create a new one.
-type LowPass struct {
+type LPF struct {
 	rc   float64
 	fc   float64
 	prev float64
 	last time.Time
 }
 
-// NewLowPass returns a new low pass filter with the specified cutoff frequency
+// NewLPF returns a new low pass filter with the specified cutoff frequency
 // in Hertz
-func NewLowPass(cutoffFreq float64) *LowPass {
-	return &LowPass{
+func NewLPF(cutoffFreq float64) *LPF {
+	return &LPF{
 		fc: cutoffFreq,
 		rc: 1 / (2 * math.Pi * cutoffFreq)}
 }
 
 // Update processes an input value, returning the filtered output
-func (l *LowPass) Update(input float64) float64 {
+func (l *LPF) Update(input float64) float64 {
 	if l.last.IsZero() {
 		// not initialized
 		l.prev = input
@@ -42,16 +42,16 @@ func (l *LowPass) Update(input float64) float64 {
 }
 
 // Cutoff returns the filter's cutoff frequency in Hertz
-func (l *LowPass) Cutoff() float64 {
+func (l *LPF) Cutoff() float64 {
 	return l.fc
 }
 
 // RC returns the filter's RC time constant, 1/(2pi cutoff)
-func (l *LowPass) RC() float64 {
+func (l *LPF) RC() float64 {
 	return l.rc
 }
 
 // Previous returns the last value at the filter's output and when it happened
-func (l *LowPass) Previous() (float64, time.Time) {
+func (l *LPF) Previous() (float64, time.Time) {
 	return l.prev, l.last
 }
